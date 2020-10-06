4c054b5a84dfaf120feaf37cf8b04ef958933e1b
Mathieu <mathieudadoun98@gmail.com>
2020-10-06 21:23:33 +0200

    test users done

--- test/users.js
+++ test/users.js
@@ -1,47 +1,52 @@
 
 const supertest = require('supertest')
 const app = require('../lib/app')
+const db = require('../lib/db')
 
 describe('users', () => {
-  
+
   beforeEach( async () => {
     await db.admin.clear()
   })
-  
-  it.skip('list empty', async () => {
+
+  it('list empty', async () => {
     // Return an empty user list by default
     const {body: users} = await supertest(app)
     .get('/users')
     .expect(200)
     users.should.match([])
   })
-  
-  it.skip('list one element', async () => {
+
+  it('list one element', async () => {
     // Create a user
     await supertest(app)
     .post('/users')
     .send({username: 'user_1'})
     // Ensure we list the users correctly
-    const {body: users} = await supertest(app)
+    const {body: user} = await supertest(app)
     .get('/users')
     .expect(200)
-    users.should.match([{
+    user.should.match([{
       id: /^\w+-\w+-\w+-\w+-\w+$/,
       username: 'user_1'
     }])
   })
-  
-  it.skip('add one element', async () => {
+
+  it('add one element', async () => {
     // Create a user
     const {body: user} = await supertest(app)
     .post('/users')
     .send({username: 'user_1'})
     .expect(201)
-    // Check its return value
+    // Check ists return value
+    user.should.match({
+      id: /^\w+-\w+-\w+-\w+-\w+$/,
+      username: 'user_1'
+    })
     // Check it was correctly inserted
     const {body: users} = await supertest(app)
     .get('/users')
     users.length.should.eql(1)
   })
-  
+
 })
