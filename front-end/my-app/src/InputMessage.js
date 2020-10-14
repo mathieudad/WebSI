5636f58217f027d951e0df30225e03c5a5cc707a
MathieuDC13 <mdecampou@gmail.com>
2020-10-14 13:30:39 +0200

    Clear input message text when send a message

--- front-end/my-app/src/InputMessage.js
+++ front-end/my-app/src/InputMessage.js
@@ -2,7 +2,7 @@ const React = require('react');
 const {useState} = require('react');
 
 function InputMessage (props){
-    const [text, setText] = useState("Message");
+    const [text, setText] = useState("");
 
     function handleTextChange(event) {
         setText(event.target.value);
@@ -17,7 +17,7 @@ function InputMessage (props){
     return(
         <div className="input-message-container">
             <button onClick={handleSendMessage}>Send</button>
-            <input className="input-message-input-text" onChange={handleTextChange} type="text" placeholder={text}/>
+            <input className="input-message-input-text" onChange={handleTextChange} type="text" onFocus={text} value={text} placeholder="Message"/>
         </div>
     )
     
