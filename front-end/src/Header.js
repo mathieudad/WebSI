8e0be8d3ca8c9e50814d96b243f79b1347f216b7
Mathieu <mathieudadoun98@gmail.com>
2020-12-19 18:07:58 +0100

    newheader

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -5,17 +5,19 @@ import { jsx } from '@emotion/core'
 // Layout
 import { useTheme } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import Context from './Context';
 import useSWR from "swr";
 import axios from "axios";
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import { ResponsiveButton } from './ResponsiveButton';
+import { Grid, Typography } from '@material-ui/core';
+
 
 const useStyles = (theme) => ({
   header: {
     padding: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.light,
-    flexShrink: 0,
+    backgroundColor: theme.palette.secondary.dark,
   },
   headerLogIn: {
     backgroundColor: 'red',
@@ -34,22 +36,38 @@ const useStyles = (theme) => ({
 
 export default () => {
   const styles = useStyles(useTheme())
-  let avatar= null
+  let avatar = null
   const {
     oauth, setOauth,
     drawerVisible, setDrawerVisible
   } = useContext(Context)
-    if(oauth){
+  if (oauth) {
     const email64 = window.btoa(oauth.email)
     const url = `http://localhost:3001/users/${email64}`;
-    
-    const fetcher = (...args) => axios.get(...args,{
+
+    const fetcher = (...args) => axios.get(...args, {
       headers: {
-      'Authorization': `Bearer ${oauth.access_token}`
-    }}).then((res) => res.data);
+        'Authorization': `Bearer ${oauth.access_token}`
+      }
+    }).then((res) => res.data);
     const { data, error } = useSWR(url, fetcher);
-    avatar =  !error && !data ? (<div>loading..</div>)
-      : (data ? (<img alt="avatar" src={data.avatar}/>): null)
+    avatar = !error && !data ? (<div>{oauth.user.name} loading..</div>)
+      : (data ? (<div>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center">
+          <Grid item>
+            <img alt="avatar" src={data.avatar} heigh="50%" width="50%" />
+          </Grid>
+          <Grid item>
+            <Typography color="textPrimary" variant="h5">
+              {oauth.user.name}
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>) : null)
   }
 
   const drawerToggle = (e) => {
@@ -59,6 +77,16 @@ export default () => {
     e.stopPropagation()
     setOauth(null)
   }
+
+
+  const buttonProps = {
+    size: "medium",
+    variant: "contained",
+    color: "primary",
+    onClick: onClickLogout
+  }
+
+
 
 
 
@@ -72,17 +100,21 @@ export default () => {
       >
         <MenuIcon />
       </IconButton>
-      Header
       {
-      oauth && oauth.user ? 
-          (<span>
-            {oauth.email}
-            <Link onClick={onClickLogout}>logout</Link>
-            {avatar}
-          </span>)
-        : (<span>new user</span>)
+        oauth && oauth.user ?
+          (<div>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center">
+              {avatar}
+              <ResponsiveButton name="logout" props={buttonProps} icon={<MeetingRoomIcon />} />
+
+            </Grid>
+          </div>)
+          : (<Typography color="textPrimary" variant="h5">Hello</Typography>)
       }
-      
     </header>
   );
 }
